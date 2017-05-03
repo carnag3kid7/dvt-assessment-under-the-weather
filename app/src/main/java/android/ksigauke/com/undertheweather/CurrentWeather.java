1fc73cb063a91e96764b4f716c7547d37f859dca
Kennedy Sigauke <KSigauke@jhbdvt.co.za>
2017-05-03 20:34:05 +0200

    Initial commit

---
+++ app/src/main/java/android/ksigauke/com/undertheweather/CurrentWeather.java
@@ -0,0 +1,13 @@
+package android.ksigauke.com.undertheweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_weather);
+    }
+}
