de0ae499a56a87e5e1d5a1be151a9f928a10aab9
Kennedy Sigauke <KSigauke@jhbdvt.co.za>
2017-05-06 17:40:46 +0200

    Add icons for current weather activity

--- app/src/main/java/android/ksigauke/com/undertheweather/utils/Constants.java
+++ app/src/main/java/android/ksigauke/com/undertheweather/utils/Constants.java
@@ -1,5 +1,19 @@
 package android.ksigauke.com.undertheweather.utils;
 
+import android.ksigauke.com.undertheweather.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constants {
-    public static final String WEATHER_UNITS ="metric";
+    public static final String WEATHER_UNITS = "metric";
+
+    public static final Map<String, Integer> FORECAST_DRAWABLES = new HashMap<>();
+
+    static {
+        FORECAST_DRAWABLES.put("Rain", R.drawable.ic_rain);
+        FORECAST_DRAWABLES.put("Cloudy", R.drawable.ic_cloudy);
+        FORECAST_DRAWABLES.put("Sunny", R.drawable.ic_sun);
+        FORECAST_DRAWABLES.put("Snow", R.drawable.ic_snow);
+    }
 }
